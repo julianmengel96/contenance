f9b7d35883c025d40859e47ba1da0532afed9a8c
Julian Mengel <julian.mengel.16@gmail.com>
2020-09-30 22:21:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    $("#menuitem").click(function() {
+        $(".menumobile").addClass("menuselected");
+        $("#closeitem").show();
+        $(".opacitylayer").addClass("opacitylayer-active");
+        $("body").css("overflow", "hidden");
+    })
+
+    $("#closeitem").click(function() {
+        $(this).hide();
+        $(".menumobile").removeClass("menuselected");
+        $(".opacitylayer").removeClass("opacitylayer-active");
+        $("body").css("overflow", "auto");
+    })
+
+    $(".opacitylayer").click(function() {
+        $("#closeitem").hide();
+        $(".menumobile").removeClass("menuselected");
+        $(".opacitylayer").removeClass("opacitylayer-active");
+    })
+
+    $("#link-products-mobile").click(function() {
+        $('.products-mobile').addClass("products-mobile-active");
+    })
+
+    $("#link-back-mobile").click(function() {
+        $('.products-mobile').removeClass("products-mobile-active");
+    })
+  
+  });
