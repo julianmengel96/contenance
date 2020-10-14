7c05d3d4d66ca65f16557bb3c8f5c2fcdddb9de5
Julian Mengel <julian.mengel.16@gmail.com>
2020-10-14 13:52:46 +0200

    aktuell

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
+var dropdown = false;
 $(document).ready(function(){
+    $(window).scroll(function() {
+        if(dropdown){
+            $(".dropdown-content").removeClass("dropdown-active");
+            $(".arrow").removeClass("arrow-active");
+            dropdown = false;
+        }
+    });
+    $(".dropdown").click(function() {
+        if(!dropdown){
+            $(".dropdown-content").addClass("dropdown-active");
+            $(".arrow").addClass("arrow-active");
+            dropdown = true;
+        }else {
+            $(".dropdown-content").removeClass("dropdown-active");
+            $(".arrow").removeClass("arrow-active");
+            dropdown = false;
+        }
+    });
+    $("#container-content").click(function() {
+        if(dropdown) {
+            $(".dropdown-content").removeClass("dropdown-active");
+            $(".arrow").removeClass("arrow-active");
+            dropdown = false;
+        }
+    })
+    $(".container-contenance").click(function() {
+        if(dropdown) {
+            $(".dropdown-content").removeClass("dropdown-active");
+            $(".arrow").removeClass("arrow-active");
+            dropdown = false;
+        }
+    })
 
     $("#menuitem").click(function() {
         $(".menumobile").addClass("menuselected");
